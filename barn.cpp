e0bb7b9faaab5fc86b0b3a0062dbd53f6121393b
Daniel-Ev-Esc <101297050+Daniel-Ev-Esc@users.noreply.github.com>
2022-04-07 09:48:18 -0500

    Archivo de prueba

---
+++ barn.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    long long int n, m, maxC;
+    int  d = 0;
+    
+    cin >> n >> m;
+
+    maxC = n;
+
+    while (n > 0){
+        d++;
+
+        if((n+m) >= maxC){
+            n = maxC;
+        }
+        else{
+            n += m;
+        }
+
+        n -= d;
+    }
+    
+    cout << d;
+}
