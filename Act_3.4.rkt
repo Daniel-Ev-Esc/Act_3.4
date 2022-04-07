56c55e1de95b6a1be5a88a113ac4f87ddd31533a
Daniel-Esc <74694850+Daniel-Esc@users.noreply.github.com>
2022-04-07 10:04:41 -0500

    Etiqueta inicial
    Recibe cualquier archivo y despliega la etiqueta inicial

--- Act_3.4.rkt
+++ Act_3.4.rkt
@@ -1,1 +1,11 @@
 #lang racket
+
+;Función que recorre un archivo, almacena sus elementos y los depliega en oteo archivo con sus definiciones
+;Complejidad: O(n)
+(define (gramati file1 file2)
+  (define p1 (open-input-file file1))
+  (define p2 (open-output-file file2))
+  (display "<!DOCTYPE html> " p2)
+  (close-output-port p2)
+  (close-input-port p1)
+  )
