d129e0bce17140c1d630e02764cb1a70bad4f948
Daniel-Esc <74694850+Daniel-Esc@users.noreply.github.com>
2022-04-07 10:12:50 -0500

    Pruebas

--- Act_3.4.rkt
+++ Act_3.4.rkt
@@ -1,9 +1,19 @@
 #lang racket
 
+
+;Función principal que llama a las demás funciones, despliega las etiquetas iniciales y finales
 (define (gramatica file1 file2)
   (define p1 (open-input-file file1))
   (define p2 (open-output-file file2))
   (display "<!DOCTYPE html> " p2)
+  (newline p2)
+  (display "<html>" p2)
+  (newline p2)
+  (display "<body>" p2)
+  (newline p2)
+  (display "</body>" p2)
+  (newline p2)
+  (display "</html>" p2)
   (close-output-port p2)
   (close-input-port p1)
   )
