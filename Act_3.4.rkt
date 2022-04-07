e30d42bed8505db78ed102e2db8db71dcf215bce
Daniel-Esc <74694850+Daniel-Esc@users.noreply.github.com>
2022-04-07 11:12:50 -0500

    Detección librerías

--- Act_3.4.rkt
+++ Act_3.4.rkt
@@ -1,12 +1,21 @@
 #lang racket
 
+(define (libreria atomo p2)
+  (display "<span style='color:yellow'>" p2)
+  (display atomo p2)
+  (display "</span>" p2))
+
+(define (coincide atomo p1 p2)
+  (if (regexp-match-exact? #rx"<(.*)>" atomo)
+      (libreria atomo p2)
+      null))
 
 
-;(define (recorre p1 p2)
- ; (if (eof-object? (peek-char p1))
-  ;    '()
-   ;   (append (list(coincide (read p1) p1 p2)) (recorre-2 p1)))
- ; null)
+;Función que recorre el archivo
+(define (recorre p1 p2)
+  (if (eof-object? (peek-char p1))
+      '()
+      (append (list(coincide (symbol->string (read p1)) p1 p2)) (recorre p1 p2))))
 
 ;Función principal que llama a las demás funciones, despliega las etiquetas iniciales y finales
 (define (compila file1 file2)
@@ -20,7 +29,7 @@
   (newline p2)
   (display "<span style='color:red'>Texto</span>" p2)
   (newline p2)
-  ;(recorre p1 p2)
+  (recorre p1 p2)
   (display "</body>" p2)
   (newline p2)
   (display "</html>" p2)
